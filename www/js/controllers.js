983b918b16d1609f3f90e347c74994ff1329457c
dianaman <diana.man.91@gmail.com>
2016-09-24 00:44:46 -0300

    movimientos, icono y splash

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,6 +15,120 @@ angular.module('starter.controllers', ['ngCordova'])
 	}
 })
 
+.controller('AudioCtrl', function($ionicPlatform, MediaSrv, $scope, $stateParams, $cordovaDeviceMotion,$timeout, $cordovaMedia){
+	try{
+		$ionicPlatform.ready(function() {
+			var izq = "/audio/izquierda.mp3";
+			var der = "/audio/derecha.mp3";
+			var atr = "/audio/atras.mp3";
+			var ade = "/audio/adelante.mp3";
+			var aba = "/audio/abajo.mp3";
+			var pr = "/audio/prueba.mp3";
+
+			var mediaIzq = $cordovaMedia.newMedia(izq);
+			var mediaDer = $cordovaMedia.newMedia(der);
+			var mediaAtr = $cordovaMedia.newMedia(atr);
+			var mediaAde = $cordovaMedia.newMedia(ade);
+			var mediaAba = $cordovaMedia.newMedia(aba);
+			var prueba = $cordovaMedia.newMedia(pr);
+			$scope.grabarIzquierda = function(){
+				console.log('Grabando izquierda');
+				mediaIzq.startRecord();
+				$timeout(function(){
+					mediaIzq.stopRecord();
+				}, 1500);
+			}
+
+			$scope.grabarDerecha = function(){
+				console.log('Grabando derecha');
+				mediaDer.startRecord();
+				$timeout(function(){
+					mediaDer.stopRecord();
+				}, 1500);
+			}
+
+			$scope.grabarAdelante = function(){
+				console.log('Grabando adelante');
+				mediaAde.startRecord();
+				$timeout(function(){
+					mediaAde.stopRecord();
+				}, 1500);
+			}
+
+			$scope.grabarAtras = function(){
+				console.log('Grabando atras');
+				mediaAtr.startRecord();
+				$timeout(function(){
+					mediaAtr.stopRecord();
+				}, 1500);
+			}
+
+			$scope.grabarAbajo = function(){
+				console.log('Grabando abajo');
+				mediaAba.startRecord();
+				$timeout(function(){
+					mediaAba.stopRecord();
+				}, 1500);
+			}
+
+			 $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+		      $scope.x = result.x;
+		      $scope.y = result.y;
+		      $scope.z = result.z;
+		      $scope.timeStamp = result.timestamp;
+		    }, function(err) {
+		    	console.log(err);
+		    });
+
+			var orientacion = '';
+
+			var options = { frequency: 4000 };
+
+			var watch = $cordovaDeviceMotion.watchAcceleration(options);
+		    watch.then(
+		      null,
+		      function(error) {
+		      	console.log(error);
+		      },
+		      function(result) {
+		      	if(9 < result.x && -5 < result.y < 5 && -5 < result.z < 5 &&
+		      		orientacion != 'izquierda'){
+		      		alert('izquierda');
+		      		MediaSrv.loadMedia('audio/prueba.mp3').then(function(media){
+					  media.play();
+					});
+		      		orientacion = 'izquierda';
+		      	}
+		      	if(-9 > result.x &&  -5 < result.y < 5 && -5 < result.z < 5 &&
+		      		orientacion != 'derecha'){
+		      		alert('derecha');
+		      		prueba.play();
+		      		orientacion = 'derecha';
+		      	}
+		      	if(9 < result.y && -5 < result.x < 5 && -5 < result.z < 5 &&
+		      		orientacion != 'adelante'){
+		      		alert('adelante');
+		      		mediaAde.play();
+		      		orientacion = 'adelante';
+		      	}
+		      	if(-9 > result.y &&  -5 < result.x < 5 && -5 < result.z < 5 &&
+		      		orientacion != 'atras'){
+		      		alert('atras');
+		      		mediaAtr.play();
+		      		orientacion = 'atras';
+		      	}
+		      	if(-9 > result.z &&  -5 < result.x < 5 && -5 < result.y < 5 &&
+		      		orientacion != 'abajo'){
+		      		alert('abajo');
+		      		mediaAtr.play();
+		      		orientacion = 'abajo';
+		      	}
+		    });
+		});
+	}catch(e){
+		console.log(e.message);
+	}
+})
 .controller('FotoCtrl', function($ionicPlatform, $scope, $stateParams, $cordovaDeviceMotion,$timeout){
 	$scope.foto={};
 	$scope.foto = JSON.parse($stateParams.foto);
@@ -68,3 +182,10 @@ angular.module('starter.controllers', ['ngCordova'])
 		console.log(e.message);
 	}
 })
+
+
+.controller('AutorCtrl', function($scope, $window) {
+  $scope.sendMail = function(emailId, subject, message){
+    $window.open("mailto:" + emailId + "?subject=" + subject+"&body="+message,"_self");
+  }
+});
